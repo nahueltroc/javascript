8ef73065abc1d9b0ae6d0951dea1f6723d64946e
Nahuel <n500nahuel@hotmail.com>
2022-11-09 23:03:32 -0300

    asad

--- proyecto.js
+++ proyecto.js
@@ -1,42 +1,54 @@
+class Personalizacion{
+    constructor(altura, peso, ataque, defensa) {
+    
+     this.altura = altura;
+    this.ataque = ataque;
+    this.defensa = defensa;
+    this.peso = peso; }
+}
+
 
 alert( "bienvenido a jueguito! seleccione una opcion");
 
 
 let personaje = parseInt (prompt("elija que personaje quiere ser \nsi te gustaria ser orco presiona 1 \nsi te gustaria ser elfo presiona 2 \nsi te gustaria ser enano presiona 3"));
-let pepe = true;
 
-switch (personaje){
-    case 1:
-        alert( "elejiste ser orco");
-        break;
-    case 2:
-        alert("elejiste ser elfo");
-        break;
-    case 3:
-        alert("elejiste ser enano");
-        break;
-}
+
+    switch (personaje){
+        case 1:
+            alert( "elejiste ser orco");
+            break;
+        case 2:
+            alert("elejiste ser elfo");
+            break;
+        case 3:
+            alert("elejiste ser enano");
+            break;
+       
+    }
+
+
 
 alert("personalizemos tu personaje, eleji la altura, ataque, peso y defensa")
 
-per = prompt("elija que quiere personalizar \n presione 1 altura, \n presione 2 ataque \n presione 3 defensa \n presione 4 peso ")
-
+let per = parseInt(prompt("elija la opcion 1 para personalizar su personaje \n elija la opcion 2 para salir"));
 let info;
+let array = [];
 
-while(0 != 4){
+ 
     switch (per){
         case 1:
             let altura = parseInt(prompt("ingrese la altura"));
+            let ataque = parseInt(prompt("ingrese el ataque"));
+            let defensa = parseInt(prompt("ingrese la defensa"));
+            let peso = parseInt(prompt("ingrese el peso"));
            info = new Personalizacion(altura, ataque, defensa, peso );
+           array.push(info);
             break;
         case 2:
-            let ataque = parseInt(prompt("ingrese el ataque"));
+           let salir = parseInt(prompt("presione 2 para salir"));
             break;
-        case 3:
-            let defensa = parseInt(prompt("ingrese la defensa"));
-            break;
-        case 4:
-            let peso = parseInt(prompt("ingrese el peso"));
-            break;
+    
     }
-}
+
+
